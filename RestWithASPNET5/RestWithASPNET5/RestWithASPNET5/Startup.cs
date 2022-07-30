75c6ac7646fed4a1febbcfc359fbeba448249efa
Maicon Moraes <maicon3000@hotmail.com>
2022-07-30 15:57:31 -0300

    Adding Cors

--- RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/Startup.cs
+++ RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/Startup.cs
@@ -42,6 +42,13 @@ namespace RestWithASPNET5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+
             services.AddControllers();
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
@@ -119,6 +126,8 @@ namespace RestWithASPNET5
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseSwagger();
 
