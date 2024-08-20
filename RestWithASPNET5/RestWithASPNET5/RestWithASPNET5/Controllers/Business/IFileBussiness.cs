da7625553f0993512284ac805b3e89caaeec2999
Maicon Moraes <maicon3000@hotmail.com>
2024-08-20 20:39:24 -0300

    Create FileBussiness, IFileBussiness and FileDetailVO

---
+++ RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/Controllers/Business/IFileBussiness.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASPNET5.Data.VO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Controllers.Business
+{
+    public interface IFileBussiness
+    {
+        public byte[] GetFile(string filename);
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailVO>> SaveFilesToDisk(List<IFormFile> file);
+    }
+}
---
+++ RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/Controllers/Business/Implementations/FileBussiness.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASPNET5.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Controllers.Business.Implementations
+{
+    public class FileBussiness : IFileBussiness
+    {
+        private readonly string _basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBussiness(IHttpContextAccessor context )
+        {
+            _context = context;
+            _basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public byte[] GetFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FileDetailVO>> SaveFilesToDisk(List<IFormFile> file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
